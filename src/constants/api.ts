bdbf257600d97eed44a791424591352ef167465d
Thoriq Afif <mthoriqafif01@gmail.com>
2024-12-19 14:59:31 +0700

    refactor: fix API constant

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -5,5 +5,6 @@ export const API_EMAIL_VALIDATION = `${API_URL}/users/email-validation`;
 export const API_CHECK_OTP = `${API_URL}/users/otp-validation`;
 export const API_REFRESH_OTP = `${API_URL}/users/otp-refresh`;
 export const API_REGISTER = `${API_URL}/users/register`;
+export const API_ALL_PRODUCT = `${API_URL}/products/search-all-product`;
 export const API_PRODUCT = `${API_URL}/products`;
-export const API_CATEGORIES = `${API_URL}/products/categories`;
+export const API_CATEGORIES = `${API_URL}/products/get-all-category`;
